cefc2e7a9b36dc0c85f16d5c73c60675961ff376
Sri Ganesh <140908088+sriganesh23@users.noreply.github.com>
2024-01-11 23:09:00 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class SelectionSort {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = obj.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
